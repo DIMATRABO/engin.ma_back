6379955bd893bd1ada34edf8a305932857ec1eef
user <dimatrabo007@gmail.com>
2025-07-20 23:10:17 +0100

    add user entity

--- deploy.sh
+++ deploy.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
+git pull 
 
-# Determine the environment based on the Git branch
-if [[ "$(git rev-parse --abbrev-ref HEAD)" == "prod" ]]; then
+# Build the Docker image
+docker build -t backend:prod .
 
+# Bring down existing containers
+docker-compose down
 
-    # Pull the latest changes from the current branch in the Git repository
-    git pull origin "$(git rev-parse --abbrev-ref HEAD)"
+# Start containers with the specified environment variables
+docker-compose up -d
 
-    # Build the Docker image
-    docker build -t backend:prod .
-
-    # Bring down existing containers
-    docker-compose down
-
-    # Start containers with the specified environment variables
-    docker-compose up -d
-
-
-else
-
-    # Pull the latest changes from the current branch in the Git repository
-    git pull origin "$(git rev-parse --abbrev-ref HEAD)"
-
-    # Build the Docker image
-    docker build -t backend:test .
-
-    # Bring down existing containers
-    docker-compose down
-
-    # Start containers with the specified environment variables
-    docker-compose up -d
-
-fi
-
+docker rmi -f `docker images -qa `
