bea018a4cb091de0cdec2b3d75d3dc1258c6805d
user <dimatrabo007@gmail.com>
2025-07-20 15:15:57 +0100

    add user entity

--- controllers/user/user_controller.py
+++ controllers/user/user_controller.py
@@ -1,6 +1,8 @@
 ''' UserController.py'''
 from flask import request
 from flask_restx import Namespace, Resource, fields
+from flask_jwt_extended import  jwt_required
+
 
 from gateways.log import Log
 from decorations.exception_handling import handle_exceptions
@@ -18,9 +20,20 @@ logger = Log()
 delete_handler = Delete()
 creating_handler = Create()
 
+
+signup_model = user_ns.model("SignUp", {
+    "username": fields.String(required=True),
+    "password": fields.String(required=True),
+    "fullname": fields.String(required=True),
+    "email": fields.String(required=True),
+    "birthdate": fields.String(required=False,description="Birth date in format YYYY-MM-DD"),
+    "address": fields.String(required=False),
+    "phoneNumber": fields.String(required=False),
+})
+
 @user_ns.route('/signup')
 class SignUp(Resource):
-    @user_ns.expect()
+    @user_ns.expect(signup_model)
     @handle_exceptions
     def post(self):
         """Create a new user"""
@@ -32,7 +45,9 @@ class SignUp(Resource):
         return UserResponseForm(response).to_dict(), 201
 
 
-'''
+delete_model = user_ns.model("DeleteUser", {
+    "id": fields.String(required=True)
+})
 
 @user_ns.route('/delete')
 class DeleteUser(Resource):
@@ -44,5 +59,3 @@ class DeleteUser(Resource):
         user_id = request.get_json().get("id")
         logger.log(f'Deleting User {user_id}')
         return delete_handler.handle(user_id), 200
-
-        '''
