ecf040bce0b51a4236bd081a23d8f1a2cd45f803
user <dimatrabo007@gmail.com>
2025-07-22 22:54:10 +0100

    user auth

--- usecases/user/auth.py
+++ usecases/user/auth.py
@@ -19,18 +19,16 @@ class Auth:
         with self.session_context as session:
             user = self.repo.get_user_by_username_or_email(session , auth_form.username_or_email)
             if not user is None:
-                if user.user_status.value == "ACTIVE":
-                    if bcrypt.checkpw(
-                        auth_form.password.encode('utf-8'),
-                        user.password.encode('utf-8')):
-                        return user
-                    else:
-                        status_message = "Bad Username or password "
-                        raise UnauthorizedException(status_message)
-                elif user.user_status.value == "PENDING":
-                    raise EmailNotVerifiedException()
-                elif user.user_status.value == "BLOCKED":
-                    raise UserBlockedException("User blocked")
+                if bcrypt.checkpw(auth_form.password.encode('utf-8'), user.password.encode('utf-8')):
+                    if user.user_status.value == "ACTIVE":
+                            return user
+                    elif user.user_status.value == "PENDING":
+                        raise EmailNotVerifiedException()
+                    elif user.user_status.value == "BLOCKED":
+                        raise UserBlockedException("User blocked")
+                else:
+                    status_message = "Bad Username or password "
+                    raise UnauthorizedException(status_message)
             else:        
                 status_message = "Bad Username or password "
                 raise UnauthorizedException(status_message)
