b3147c16ac47db5030bcf30d9e250d60837c0286
user <dimatrabo007@gmail.com>
2025-08-08 00:06:22 +0100

    test equipment image saving

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 sleep 5
-alembic downgrade base
-alembic upgrade 190720252148 #initial migration
+#alembic downgrade base
+#alembic upgrade 190720252148 #initial migration
 #alembic downgrade b0c7ba5d9a9f
 alembic upgrade head
 gunicorn -w 4 -b 0.0.0.0:${APP_PORT} app:app --timeout 120 --worker-class gevent --log-level debug 
