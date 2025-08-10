cebd99f38b41d4df0deadf9754dedb6aab12ad08
user <dimatrabo007@gmail.com>
2025-08-10 12:23:04 +0100

    test equipment image saving

--- config/api_config.py
+++ config/api_config.py
@@ -2,11 +2,13 @@
 from flask_restx import Api
 from config.config_handler import ConfigHandler
 from controllers.health_check_controller import healthcheck_ns
+from controllers.contact_us.contact_us import contact_ns
 from controllers.user.user_controller import user_ns
 from controllers.city.city_controller import city_ns
 from controllers.brand.brand_controller import brand_ns
 from controllers.model.model_controller import model_ns
 from controllers.equipment_image.equipment_image_controller import equipment_image_ns
+
 
 
 
@@ -46,9 +48,11 @@ def register_namespaces(api):
     """Register all API namespaces."""
 
     api.add_namespace(healthcheck_ns, path="/health")
+    api.add_namespace(contact_ns, path="/contact")
     api.add_namespace(user_ns, path="/users")
     api.add_namespace(city_ns, path="/cities")
     api.add_namespace(brand_ns, path="/brands")
     api.add_namespace(model_ns, path="/models")
     api.add_namespace(equipment_image_ns, path="/equipment-images")
+
     #api.add_namespace(auth_ns, path="/auth")
