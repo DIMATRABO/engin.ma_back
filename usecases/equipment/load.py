4ee280c7a120ff71e4fea4b8f2556e4457bcddc0
user <dimatrabo007@gmail.com>
2025-09-03 06:44:29 +0100

    debug

--- usecases/equipment/load.py
+++ usecases/equipment/load.py
@@ -1,24 +1,24 @@
 
 ''' Use case for loading equipment details by id '''
 from models.equipment import Equipment
-from gateways.dataBaseSession.session_context import SessionContext
-from gateways.equipment.repository import Repository as EquipmentRepository
-from gateways.user.repository import Repository as UserRepository
-from gateways.brand.repository import Repository as BrandRepository
-from gateways.model.repository import Repository as ModelRepository
-from gateways.city.repository import Repository as CityRepository
 
+from usecases.city.get_by_id import GetById as GetCityById
+from usecases.brand.get_by_id import GetById as GetBrandById
+from usecases.user.details import Details as GetUserById
+from usecases.model.get_by_id import GetById as GetModelById
+from usecases.equipment_image.get_all_by_equipment_id import GetAllByEquipmentId
+
+from gateways.dataBaseSession.session_context import SessionContext
 
 class Load:
     ''' retrieve equipment details by id use case '''
     def __init__(self):
         ''' initialize the Load use case with user and equipment repositories '''
-        self.user_repo= UserRepository()
-        self.equipment_repo= EquipmentRepository()
-        self.brand_repo= BrandRepository()
-        self.model_repo= ModelRepository()
-        self.city_repo= CityRepository()
-
+        self.city_getter = GetCityById()
+        self.brand_getter = GetBrandById()
+        self.model_getter = GetModelById()
+        self.user_getter = GetUserById()
+        self.images_getter = GetAllByEquipmentId()
         self.session_context = SessionContext()
 
     def handle(self,session, equipment:Equipment)->Equipment:
@@ -26,25 +26,27 @@ class Load:
         if not session:
             with self.session_context as session:
                 if equipment.owner and equipment.owner.id:
-                    equipment.owner= self.user_repo.get_user_by_id(session, equipment.owner.id)
+                    equipment.owner= self.user_getter.handle(equipment.owner.id)
                 if equipment.pilot and equipment.pilot.id:
-                    equipment.pilot= self.user_repo.get_user_by_id(session, equipment.pilot.id)
+                    equipment.pilot= self.user_getter.handle(equipment.pilot.id)
                 if equipment.brand and equipment.brand.id:
-                    equipment.brand= self.brand_repo.get_by_id(session, equipment.brand.id)
+                    equipment.brand= self.brand_getter.handle(equipment.brand.id)
                 if equipment.model and equipment.model.id:
-                    equipment.model= self.model_repo.get_by_id(session, equipment.model.id)
+                    equipment.model= self.model_getter.handle(equipment.model.id)
                 if equipment.city and equipment.city.id:
-                    equipment.city= self.city_repo.get_by_id(session, equipment.city.id)
+                    equipment.city= self.city_getter.handle(equipment.city.id)
+                equipment.images= self.images_getter.handle(equipment.id)
             return equipment
         else:
             if equipment.owner and equipment.owner.id:
-                equipment.owner= self.user_repo.get_user_by_id(session, equipment.owner.id)
+                equipment.owner= self.user_getter.handle(equipment.owner.id)
             if equipment.pilot and equipment.pilot.id:
-                equipment.pilot= self.user_repo.get_user_by_id(session, equipment.pilot.id)
+                equipment.pilot= self.user_getter.handle(equipment.pilot.id)
             if equipment.brand and equipment.brand.id:
-                equipment.brand= self.brand_repo.get_by_id(session, equipment.brand.id)
+                equipment.brand= self.brand_getter.handle(equipment.brand.id)
             if equipment.model and equipment.model.id:
-                equipment.model= self.model_repo.get_by_id(session, equipment.model.id)
+                equipment.model= self.model_getter.handle(equipment.model.id)
             if equipment.city and equipment.city.id:
-                equipment.city= self.city_repo.get_by_id(session, equipment.city.id)
-            return equipment
+                equipment.city= self.city_getter.handle(equipment.city.id)
+            equipment.images= self.images_getter.handle(equipment.id)
+        return equipment
